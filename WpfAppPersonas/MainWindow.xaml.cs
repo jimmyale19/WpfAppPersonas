e02e485bf82d75707be03de48f46ea61f7cb57be
jimmy <jimmy@JIMMY>
2025-07-03 04:45:18 -0300

    Add project files.

---
+++ WpfAppPersonas/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Windows;
+using WpfAppPersonas.Models; //  Esto importa el modelo
+
+namespace WpfAppPersonas
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CargarPersonas(); //Carga los datos al abrir
+        }
+
+        private void CargarPersonas()
+        {
+            using (var context = new PersonasDBEntities())
+            {
+                var personas = context.Personas.ToList();
+                PersonasDataGrid.ItemsSource = personas;
+            }
+        }
+    }
+}
